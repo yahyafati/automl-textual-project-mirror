ba2c21eb03a506b6c95f9a8efc5e6fb67a2e8474
Yahya Fati <yfati037@gmail.com>
2026-07-24 14:49:39 +0200

    Refactor import paths for optimizers to align with new directory structure

--- automl/core/optimizers/__init__.py
+++ automl/core/optimizers/__init__.py
@@ -1,6 +1,6 @@
 from .ifbo import IfboOptimizer
-from .random import RandomSearch
-from .rl_freeze_thaw import RLFreezeThawOptimizer
-from .smac import SmacOptimizer
+from automl.core.optimizers.baselines.random import RandomSearch
+from automl.core.optimizers.baselines.rl_freeze_thaw import RLFreezeThawOptimizer
+from automl.core.optimizers.baselines.smac import SmacOptimizer
 
 __all__ = ["RandomSearch", "SmacOptimizer", "RLFreezeThawOptimizer", "IfboOptimizer"]
